c3f86d379cadd92fa654279004b99e24eab83805
ruthloki <lokeshlugu@gmail.com>
2025-04-09 14:34:34 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ProfileCard from './ProfileCard'
+import GridView from './GridView'
+import Todo from './Todo'
+import Navbar from './Navbar'
+import Home from './Home'
+import store from './storeredux'
+import { Provider } from 'react-redux'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const Profilelist = {
+  name: "LOKESH",
+  Department: "AIDS",
+  year: 2,
+  mobile: 8124232455,
+  address: "D.No: 856,North street,Nainapalayam",
+};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +25,28 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+        {/* <Route path="/todo" element={<Todo />} /> */}
+        <Route path="/" element={<Home/>} />
+        <Route path="/profilecard" element={<ProfileCard profile= {Profilelist} />} />
+        <Route path="/gridview" element={<GridView />} />
+        <Route
+        path="/reduxcounter"
+        element={
+          <Provider store={store}>
+            <Todo/>
+          </Provider>
+        }
+        />
+        </Routes>
+        </BrowserRouter>
+        {/* <ProfileCard profile= {Profilelist}/>
+        <GridView/>
+        <Todo/>
+         */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
